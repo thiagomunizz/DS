f539d79e1117516fa94308e753468ecceecbabcf
Thiago Muniz <36748831+thiagomunizz@users.noreply.github.com>
2018-04-03 22:44:30 -0300

    Exemplos

---
+++ Exemplos/Exemplo6/Exemplo6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+            double peso;
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine(nome + ", digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());         //ou idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(nome + ", digite seu peso: "); 
+            peso = double.Parse(Console.ReadLine());        //ou peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n***Informações digitadas*** \n");
+            Console.WriteLine("{0} tem {1} anos de idades e pesa {2:N3}kg", nome, idade, peso);
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Exemplos/Exemplo8/Exemplo8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b;
+
+            Console.WriteLine("Exemplo - Operadores Relacionais");
+            Console.WriteLine("********************************* \n\n");
+
+            Console.Write("Digite um número: ");        a = int.Parse(Console.ReadLine());
+            Console.Write("Digite outro número: ");     b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("{0} é igual a {1}?           Resposta: {2}", a, b, (a == b));
+            Console.WriteLine("{0} é diferente de {1}?      Resposta: {2}", a, b, (a != b));
+            Console.WriteLine("{0} é maior que {1}?         Resposta: {2}", a, b, (a > b));
+            Console.WriteLine("{0} é menor que {1}?         Resposta: {2}", a, b, (a < b));
+            Console.WriteLine("{0} é maior ou igual a {1}?  Resposta: {2}", a, b, (a >= b));
+            Console.WriteLine("{0} é menor ou igual a {1}?  Resposta: {2}", a, b, (a <= b));
+
+            Console.ReadKey();
+        } 
+    }
+}
